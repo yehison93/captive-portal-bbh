002771471ce4d468e8bd481f67c0434b6ee4f3fb
Yehison Perdomo <yehison93@gmail.com>
2024-10-23 18:02:14 -0400

    "Remove margin and padding classes from paragraph element in Footer component"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
   return (
     <footer className="container-footer text-light">
-      <p className="m-0 p-0">
+      <p>
         &copy; {currentYear}{" "}
         <strong className="text-warning">Maremares Hotel</strong>. Todos los
         derechos reservados.
