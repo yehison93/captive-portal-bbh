245d943ee729f81e228ee1dd80bd733ca3ad1337
Yehison Perdomo <yehison93@gmail.com>
2024-10-23 21:31:11 -0400

    Remove vite-plugin-image-optimizer and svgo dependencies, and simplify vite.config.js by removing image optimization plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
-      includePublic: true,
-      logStats: true,
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: { cleanupNumericValues: false, removeViewBox: false },
-            },
-          },
-          "sortAttrs",
-          {
-            name: "addAttributesToSVGElement",
-            params: { attributes: [{ xmlns: "http://www.w3.org/2000/svg" }] },
-          },
-        ],
-      },
-      png: { quality: 100 },
-      jpeg: { quality: 100 },
-    }),
-  ],
+  plugins: [react()],
 });
