026fda0dac4bf25fdaf6ace7ab77d65b5713b920
Yehison Perdomo <yehison93@gmail.com>
2024-10-23 20:00:54 -0400

    "Update backend URL in fetch request"

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     try {
       const response = await fetch(
         //backend alojado en render
-        "https://backend-portal-captive-bbh-1.onrender.com/connect",
+        "https://backend-portal-captive-bbh.onrender.com/connect",
         {
           method: "POST",
           headers: {
