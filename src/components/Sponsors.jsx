9125d9b3c1c54c73f2de0da6a80477c462899fa4
Yehison Perdomo <yehison93@gmail.com>
2024-12-06 10:03:40 -0400

    "Removed sponsor 'Los Gloriosos Navegantes del Magallanes' from Sponsors component"

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -3,17 +3,17 @@ import { Carousel, Figure } from "react-bootstrap";
 import logoTecnonet from "../assets/sponsors/tecnonet.webp";
 import logoBBH from "../assets/sponsors/bbh.png";
 import logoBrisas from "../assets/sponsors/brisas.png";
-import logoInvitado01 from "../assets/sponsors/logoInvitado04.png";
+// import logoInvitado01 from "../assets/sponsors/logoInvitado04.png";
 import logoInvitado02 from "../assets/sponsors/Oriente-2024-H.png";
 import logoInvitado03 from "../assets/sponsors/logoInvitado03.png";
 import logoInvitado04 from "../assets/sponsors/logoInvitado01.svg";
 
 const sponsorItem = [
-  {
-    name: "Los Gloriosos Navegantes del Magallanes",
-    img: logoInvitado01,
-    slogan: "¡Bienvenidos!",
-  },
+  // {
+  //   name: "Los Gloriosos Navegantes del Magallanes",
+  //   img: logoInvitado01,
+  //   slogan: "¡Bienvenidos!",
+  // },
   {
     name: "La Guaira",
     img: logoInvitado04,
