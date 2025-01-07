4b324666179a14fc4a61452e1b6dc03fca02c8ef
Yehison Perdomo <yehison93@gmail.com>
2025-01-07 18:54:39 -0400

    `Refactor navigation and connection buttons in PortalCautive and App components`

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const App = () => {
         setLoading(false);
         // const webUrl = "https://www.instagram.com/maremareshotel/";
 
-        // window.location.href = "https://www.instagram.com/maremareshotel/";
+        window.location.replace("https://www.instagram.com/maremareshotel/");
       } else {
         setMessage(
           `Hubo un problema al conectarse, intenta de nuevo mas tarde.`
