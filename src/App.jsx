22b9382195495cf6c0324940dd50af532fcf292e
Yehison Perdomo <yehison93@gmail.com>
2025-01-07 18:08:58 -0400

    `Updated App.jsx to change redirect behavior after login attempt`

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,8 @@ const App = () => {
         setLoading(false);
         // const webUrl = "https://www.instagram.com/maremareshotel/";
 
-        window.location.href = "https://www.instagram.com/maremareshotel/";
+        // window.location.href = "https://www.instagram.com/maremareshotel/";
+        window.location.reload();
       } else {
         setMessage(
           `Hubo un problema al conectarse, intenta de nuevo mas tarde.`
