50e49f98e31755e3cd8ac4251dedf61893456f7a
Yehison Perdomo <yehison93@gmail.com>
2025-02-13 13:20:44 -0400

    `Fixed Button visibility conditions in PortalCautive component`

--- src/components/PortalCautive.jsx
+++ src/components/PortalCautive.jsx
@@ -62,7 +62,7 @@ const PortalCautive = ({
                   !connected &&
                   (isOnline ? (
                     <Button
-                      hidden={isReady}
+                      hidden={!isReady}
                       className="btn-submit"
                       variant="light"
                       href="http://www.instagram.com/maremareshotel/"
@@ -71,7 +71,7 @@ const PortalCautive = ({
                     </Button>
                   ) : (
                     <Button
-                      hidden={isReady}
+                      hidden={!isReady}
                       className="btn-submit"
                       variant="light"
                       onClick={() => handleConnect(8000, 8000, 10080)}
