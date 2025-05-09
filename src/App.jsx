d6ab783841ad1c93c3a87a715399cfe3fdb4678e
Yehison Perdomo <yehison93@gmail.com>
2025-05-09 19:50:05 -0400

    Actualiza la URL de conexión en UnifiData a la dirección correcta del hotel.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PortalCautive from "./components/PortalCautive";
 import "./App.css";
 
 const UnifiData = {
-  url: "https://buddhabarbeachhotel.ddns.net:8443",
+  url: "https://hotelmaremareswifi.loca.lt/",
   siteID: "d41gke5t",
   userName: "API.Admin",
   pw: "123456BBH#",
