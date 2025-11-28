1543022e8a3af080d684bebf7ec4d5b1e6a18f38
Yehison Perdomo <142647337+yehison93@users.noreply.github.com>
2025-11-27 22:17:33 -0400

    change url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PortalCautive from "./components/PortalCautive";
 import "./App.css";
 
 const UnifiData = {
-  url: "https://buddhabarbeachhotel.ddns.net:8443",
+  url: "https://unifimaremares.share.zrok.io",
   siteID: "d41gke5t",
   userName: "API.Admin",
   pw: "123456BBH#",
